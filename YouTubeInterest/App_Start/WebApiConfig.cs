517f5573ac17d4c30bf6e918f810226d8d81868e
wadhwa94 <wadhwajatin94@gmail.com>
2018-04-09 19:59:49 +0530

    cors origin and web config with nuget packages

--- YouTubeInterest/App_Start/WebApiConfig.cs
+++ YouTubeInterest/App_Start/WebApiConfig.cs
@@ -5,6 +5,7 @@ using System.Net.Http;
 using System.Web.Http;
 using Microsoft.Owin.Security.OAuth;
 using Newtonsoft.Json.Serialization;
+using System.Web.Http.Cors;
 
 namespace YouTubeInterest
 {
@@ -25,6 +26,9 @@ namespace YouTubeInterest
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            EnableCorsAttribute cors = new EnableCorsAttribute("http://localhost:63890 , http://youtubeinterest.azurewebsites.net/ https://youtubeinterest.azurewebsites.net/ ", "*", "*");
+            config.EnableCors(cors);
         }
     }
 }
