75f4247086fa98f15e0b250ca63f3062b3be5ccb
wadhwa94 <wadhwajatin94@gmail.com>
2018-04-10 12:29:16 +0530

    playing around

--- YouTubeInterest/YouTubeApi.cs
+++ YouTubeInterest/YouTubeApi.cs
@@ -83,6 +83,11 @@ namespace YouTubeInterest
             string message = "";
             try
             {
+                VidCategory_Array.Add(ytService.Name, 1);
+                VidCategory_Array.Add(ytService.ApiKey,2);
+                VidCategory_Array.Add(ytService.VideoCategories.List("snippet").ToString(), 3);
+
+
                 //message = message + "1 ";
                 //var channelsRequest = ytService.Channels.List("contentDetails");
                 //message = message + "2 ";
