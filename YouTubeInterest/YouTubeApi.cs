23e8c0fa7fba6e31407349bd65428fe627d74265
wadhwa94 <wadhwajatin94@gmail.com>
2018-04-10 12:21:46 +0530

    commenting the error code

--- YouTubeInterest/YouTubeApi.cs
+++ YouTubeInterest/YouTubeApi.cs
@@ -83,76 +83,76 @@ namespace YouTubeInterest
             string message = "";
             try
             {
-                message = message + "1 ";
-                var channelsRequest = ytService.Channels.List("contentDetails");
-                message = message + "2 ";
-                channelsRequest.Mine = true;
-                message = message + "3 ";
-                var likePlaylistIIdResponse = channelsRequest.Execute();
-                message = message + "4 ";
-                Google.Apis.YouTube.v3.Data.PlaylistItemListResponse videosResponse = null;
-                message = message + "5 ";
-                int currentVideoCount = 0;
-                message = message + "6 ";
-                if (likePlaylistIIdResponse.Items.Count > 0)
-                {
-                    string likePlayListId = likePlaylistIIdResponse.Items[0].ContentDetails.RelatedPlaylists.Likes;
-                    string nextPagetoken = null;
-                    do
-                    {
-                        var videosRequest = ytService.PlaylistItems.List("snippet");
-                        videosRequest.PlaylistId = likePlayListId;
-                        if (videoCount != -1)
-                            videosRequest.MaxResults = Math.Min(videoCount, 50);
-                        else
-                            videosRequest.MaxResults = 50;
-                        if (nextPagetoken != null)
-                        {
-                            if (videosResponse.NextPageToken != null)
-                            {
-                                videosRequest.PageToken = videosResponse.NextPageToken;
-                            }
-                        }
-                        videosResponse = videosRequest.Execute();
-                        currentVideoCount += videosResponse.Items.Count;
-                        for (int i = 0; i < videosResponse.Items.Count; i++)
-                        {
+                //message = message + "1 ";
+                //var channelsRequest = ytService.Channels.List("contentDetails");
+                //message = message + "2 ";
+                //channelsRequest.Mine = true;
+                //message = message + "3 ";
+                //var likePlaylistIIdResponse = channelsRequest.Execute();
+                //message = message + "4 ";
+                //Google.Apis.YouTube.v3.Data.PlaylistItemListResponse videosResponse = null;
+                //message = message + "5 ";
+                //int currentVideoCount = 0;
+                //message = message + "6 ";
+                //if (likePlaylistIIdResponse.Items.Count > 0)
+                //{
+                //    string likePlayListId = likePlaylistIIdResponse.Items[0].ContentDetails.RelatedPlaylists.Likes;
+                //    string nextPagetoken = null;
+                //    do
+                //    {
+                //        var videosRequest = ytService.PlaylistItems.List("snippet");
+                //        videosRequest.PlaylistId = likePlayListId;
+                //        if (videoCount != -1)
+                //            videosRequest.MaxResults = Math.Min(videoCount, 50);
+                //        else
+                //            videosRequest.MaxResults = 50;
+                //        if (nextPagetoken != null)
+                //        {
+                //            if (videosResponse.NextPageToken != null)
+                //            {
+                //                videosRequest.PageToken = videosResponse.NextPageToken;
+                //            }
+                //        }
+                //        videosResponse = videosRequest.Execute();
+                //        currentVideoCount += videosResponse.Items.Count;
+                //        for (int i = 0; i < videosResponse.Items.Count; i++)
+                //        {
 
-                            var videoId = videosResponse.Items[i].Snippet.ResourceId.VideoId; //itearate for all the videos
+                //            var videoId = videosResponse.Items[i].Snippet.ResourceId.VideoId; //itearate for all the videos
 
-                            var videoDetails = ytService.Videos.List("snippet");
-                            videoDetails.Id = videoId;
-                            var videoDetailsResponse = videoDetails.Execute();
-                            if (videoDetailsResponse.Items.Count > 0)
-                            {
-                                var videoCategoryId = videoDetailsResponse.Items[0].Snippet.CategoryId;
-                                var videoCategoryNameRequest = ytService.VideoCategories.List("snippet");
-                                videoCategoryNameRequest.Id = videoCategoryId;
-                                var videoCategoryNameResponse = videoCategoryNameRequest.Execute();
-                                var videoCategoryName = videoCategoryNameResponse.Items[0].Snippet.Title;
+                //            var videoDetails = ytService.Videos.List("snippet");
+                //            videoDetails.Id = videoId;
+                //            var videoDetailsResponse = videoDetails.Execute();
+                //            if (videoDetailsResponse.Items.Count > 0)
+                //            {
+                //                var videoCategoryId = videoDetailsResponse.Items[0].Snippet.CategoryId;
+                //                var videoCategoryNameRequest = ytService.VideoCategories.List("snippet");
+                //                videoCategoryNameRequest.Id = videoCategoryId;
+                //                var videoCategoryNameResponse = videoCategoryNameRequest.Execute();
+                //                var videoCategoryName = videoCategoryNameResponse.Items[0].Snippet.Title;
 
-                                if (VidCategory_Array.ContainsKey(videoCategoryName))
-                                {
-                                    VidCategory_Array[videoCategoryName] += 1;
-                                }
-                                else
-                                {
-                                    VidCategory_Array.Add(videoCategoryName, 1);
-                                }
-                            }
+                //                if (VidCategory_Array.ContainsKey(videoCategoryName))
+                //                {
+                //                    VidCategory_Array[videoCategoryName] += 1;
+                //                }
+                //                else
+                //                {
+                //                    VidCategory_Array.Add(videoCategoryName, 1);
+                //                }
+                //            }
 
-                        }
+                //        }
 
-                        if (videosResponse.NextPageToken != null)
-                            nextPagetoken = videosResponse.NextPageToken;
-                    }while (videosResponse.NextPageToken != null && videoCount == -1);
-                }
+                //        if (videosResponse.NextPageToken != null)
+                //            nextPagetoken = videosResponse.NextPageToken;
+                //    }while (videosResponse.NextPageToken != null && videoCount == -1);
+                //}
 
                 return VidCategory_Array;
             }
             catch (Exception e)
             {
-                VidCategory_Array.Add("ytService.BatchUri = " + ytService.BatchUri + ytService.Videos.List("content")    + e.Message.ToString() + " " + message + " ----  " + e.InnerException, -1);
+                VidCategory_Array.Add("ytService = " + ytService   + e.Message.ToString() + " " + message + " ----  " + e.InnerException, -1);
                 return VidCategory_Array;
             }
              
