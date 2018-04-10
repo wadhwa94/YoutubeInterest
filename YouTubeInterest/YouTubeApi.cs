05ac6e94ff8704a63dab91a671fddead9884674e
wadhwa94 <wadhwajatin94@gmail.com>
2018-04-10 13:31:20 +0530

    playing around

--- YouTubeInterest/YouTubeApi.cs
+++ YouTubeInterest/YouTubeApi.cs
@@ -56,7 +56,7 @@ namespace YouTubeInterest
                         new[] { YouTubeService.Scope.YoutubeReadonly, YouTubeService.Scope.Youtube, YouTubeService.Scope.YoutubeForceSsl, YouTubeService.Scope.Youtubepartner, YouTubeService.Scope.YoutubeUpload },
                         DateTime.Now.Ticks.ToString(), //this string needs to be changed for all the users else it will take the previous one or don't know which one
                         CancellationToken.None,
-                        new FileDataStore("Drive.Auth.Store")
+                        new FileDataStore("YouTubeHistory")
                     ).Result;
             }
 
